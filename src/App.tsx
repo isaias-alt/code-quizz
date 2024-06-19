3bd2a0f8a23685c16be4cfea674fc52bcde061c6
isaias-at <cascolucasisaias@gmail.com>
2024-06-19 16:07:01 -0400

    initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Container, Stack, Typography } from '@mui/material'
 import './App.css'
+import { JavaScriptLogo } from './JavaScriptLogo'
 
 function App() {
   return (
-    <h1>JS-Quiz</h1>
+    <main>
+      <Container maxWidth='sm'>
+        <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+          <JavaScriptLogo />
+          <Typography variant='h2' component='h1'>
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
   )
 }
 
