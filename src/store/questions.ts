4b3dfa69cb7639e3666ecd6189d13fa255fb8495
isaias-at <cascolucasisaias@gmail.com>
2024-06-20 01:10:07 -0400

    add questions store

---
+++ src/store/questions.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { Question } from "../types.d";
+
+interface State {
+  questions: Question[];
+  currentQuestion: number;
+  fetchQuestions: (limit: number) => Promise<void>;
+}
+
+export const useQuestionsStore = create<State>((set) => {
+  return {
+    questions: [],
+    currentQuestion: 0,
+    fetchQuestions: async (limit: number) => {
+      const res = await fetch('http://localhost:5173/data.json')
+      const data = await res.json()
+
+      const questions = data.sort(() => Math.random() - 0.5).slice(0, limit)
+
+      set({ questions })
+    },
+  };
+});
