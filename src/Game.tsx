a713b66912ac4e39255079d5e7926bd4e5c6d6fe
isaias-at <cascolucasisaias@gmail.com>
2024-06-20 18:04:04 -0400

    work on Game component

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,5 +1,32 @@
-export const Game = () => {
+import { Card, IconButton, Stack, Typography } from "@mui/material"
+import { useQuestionsStore } from "./store/questions"
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { type Question as QuestionType } from "./types.d"
+
+const Question = ({ info }: {info: QuestionType}) => {
   return (
-    <h1>Game</h1>
+    <Card variant="outlined" sx={{ textAlign: 'left', bgcolor: '#222', p: 2 }}>
+      <Typography variant="h5">
+        {info.question}
+      </Typography>
+      <SyntaxHighlighter language="JavaScript" style={atomOneDark}>
+        {info.code}
+      </SyntaxHighlighter>
+    </Card>
+  )
+}
+
+export const Game = () => {
+  const questions = useQuestionsStore(state => state.questions)
+  const currentQuestion = useQuestionsStore(state => state.currentQuestion)
+
+  const questionInfo = questions[currentQuestion]
+
+currentQuestion
+  return (
+    <>
+      <Question info={questionInfo}/>
+    </>
   )
 }
