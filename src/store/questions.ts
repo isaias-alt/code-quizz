d63e90e3dfc559f60ff0c6298052d77b747d9a10
isaias-at <cascolucasisaias@gmail.com>
2024-06-22 16:12:16 -0400

    add select answer store

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -2,22 +2,36 @@ import { create } from "zustand";
 import { Question } from "../types.d";
 
 interface State {
-  questions: Question[];
-  currentQuestion: number;
-  fetchQuestions: (limit: number) => Promise<void>;
+  questions: Question[]
+  currentQuestion: number
+  fetchQuestions: (limit: number) => Promise<void>
+  selectAnswer: (questionId: number, answerIndex: number) => void
 }
 
-export const useQuestionsStore = create<State>((set) => {
+export const useQuestionsStore = create<State>((set, get) => {
   return {
     questions: [],
     currentQuestion: 0,
-    fetchQuestions: async (limit: number) => {
+    fetchQuestions: async (limit) => {
       const res = await fetch('http://localhost:5173/data.json')
       const data = await res.json()
-
       const questions = data.sort(() => Math.random() - 0.5).slice(0, limit)
-
       set({ questions })
+    },
+    selectAnswer: (questionId, answerIndex) => {
+      const { questions } = get()
+      const newQuestions = structuredClone(questions)
+      const questionIndex = newQuestions.findIndex(question => question.id === questionId)
+      const questionInfo =  newQuestions[questionIndex]
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+
+      newQuestions[questionIndex] = {
+        ...questionInfo,
+        isCorrectUserAnswer,
+        userSelectedAnswer: answerIndex
+      }
+      
+      set({ questions: newQuestions })
     },
   };
 });
