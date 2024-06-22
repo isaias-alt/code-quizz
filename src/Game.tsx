9a344d68fe964ee9eed101343c4bbc3c706eba97
isaias-at <cascolucasisaias@gmail.com>
2024-06-22 17:20:34 -0400

    work in game

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,8 +1,10 @@
-import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material"
+import { Card, Stack, IconButton, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material"
+import ArrowBackIos from '@mui/icons-material/ArrowBackIosNew';
 import { useQuestionsStore } from "./store/questions"
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import { type Question as QuestionType } from "./types.d"
+import { ArrowForwardIos } from "@mui/icons-material";
 
 const getBackGroundColor = (info: QuestionType, index: number) => {
   const { userSelectedAnswer, correctAnswer } = info
@@ -26,7 +28,7 @@ const Question = ({ info }: {info: QuestionType}) => {
   }
   
   return (
-    <Card variant="outlined" sx={{ textAlign: 'left', bgcolor: '#222', p: 2, marginTop: 4 }}>
+    <Card variant="outlined" sx={{ textAlign: 'left', bgcolor: '#222', p: 2, marginTop: 2 }}>
       <Typography variant="h5" marginBottom={2}>
         {info.question}
       </Typography>
@@ -56,12 +58,23 @@ const Question = ({ info }: {info: QuestionType}) => {
 export const Game = () => {
   const questions = useQuestionsStore(state => state.questions)
   const currentQuestion = useQuestionsStore(state => state.currentQuestion)
+  const goNextQuestion = useQuestionsStore(state => state.goNextQuestion)
+  const goPreviousQuestion = useQuestionsStore(state => state.goPreviousQuestion)
 
   const questionInfo = questions[currentQuestion]
 
 currentQuestion
   return (
     <>
+      <Stack direction="row" gap={2} alignItems="center" justifyContent="center" >
+        <IconButton onClick={goPreviousQuestion} disabled={currentQuestion === 0}>
+          <ArrowBackIos />
+        </IconButton>
+        {currentQuestion + 1} / {questions.length}
+        <IconButton onClick={goNextQuestion} disabled={currentQuestion >= questions.length - 1}>
+          <ArrowForwardIos />
+        </IconButton>
+      </Stack>
       <Question info={questionInfo}/>
     </>
   )
