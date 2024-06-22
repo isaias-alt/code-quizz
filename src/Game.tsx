21bea7a2a025ae46451b81f83fb663dba1a61746
isaias-at <cascolucasisaias@gmail.com>
2024-06-22 16:46:31 -0400

    add selectable answer and color for better looking

--- src/Game.tsx
+++ src/Game.tsx
@@ -4,7 +4,27 @@ import SyntaxHighlighter from 'react-syntax-highlighter'
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import { type Question as QuestionType } from "./types.d"
 
+const getBackGroundColor = (info: QuestionType, index: number) => {
+  const { userSelectedAnswer, correctAnswer } = info
+
+  if (userSelectedAnswer == null) return 'transparent'
+
+  if(index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+
+  if (index === correctAnswer) return '#258525'
+
+  if (index === userSelectedAnswer) return '#8a1919'
+
+  return 'transparent'
+}
+
 const Question = ({ info }: {info: QuestionType}) => {
+  const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+
+  const handleClick = (answerIndex: number) => () => {
+    selectAnswer(info.id, answerIndex)
+  }
+  
   return (
     <Card variant="outlined" sx={{ textAlign: 'left', bgcolor: '#222', p: 2, marginTop: 4 }}>
       <Typography variant="h5" marginBottom={2}>
@@ -17,7 +37,13 @@ const Question = ({ info }: {info: QuestionType}) => {
       <List sx={{bgcolor: '#333'}} disablePadding>
         {info.answers.map((answer, index) => (
           <ListItem key={index} disablePadding divider>
-            <ListItemButton>
+            <ListItemButton 
+              onClick={handleClick(index)}
+              disabled={info.userSelectedAnswer != null}
+              sx={{
+                backgroundColor: getBackGroundColor(info, index)
+              }}
+            >
               <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
             </ListItemButton>
           </ListItem>
