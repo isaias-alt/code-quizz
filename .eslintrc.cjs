d14753e962047945d3cabf8a78da14258457f166
isaias-at <cascolucasisaias@gmail.com>
2024-06-22 20:33:36 -0400

    improve linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,12 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {},
 }
