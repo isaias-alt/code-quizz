9114c785d995943ae3b2d1b8849055a7a033d8e9
isaias-at <cascolucasisaias@gmail.com>
2024-06-24 13:02:38 -0400

    rename data

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,7 +1,7 @@
 import { Question } from "../types.d";
 
 export const getAllQuestions = async (limit: number): Promise<Question[]> => {
-  const res = await fetch('http://localhost:5173/data.json');
+  const res = await fetch('http://localhost:5173/javascript.json');
   const data: Question[] = await res.json();
   return data.sort(() => Math.random() - 0.5).slice(0, limit);
 };
