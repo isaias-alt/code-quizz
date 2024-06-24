2318f7c7a913c06462d5d3c0202e7ce160dbcbbe
isaias-at <cascolucasisaias@gmail.com>
2024-06-23 20:04:36 -0400

    delete spaces

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -8,15 +8,10 @@ import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from "
 
 const getBackGroundColor = (info: QuestionType, index: number) => {
   const { userSelectedAnswer, correctAnswer } = info
-
   if (userSelectedAnswer == null) return 'transparent'
-
   if(index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
-
   if (index === correctAnswer) return '#258525'
-
   if (index === userSelectedAnswer) return '#8a1919'
-
   return 'transparent'
 }
 
