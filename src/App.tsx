87b274bd8901ad50166c9237d8eaa8f5d5e1d767
isaias-at <cascolucasisaias@gmail.com>
2024-06-24 12:54:38 -0400

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,36 @@
-import { Container, MenuItem, Select, Stack, Typography } from '@mui/material'
-import './App.css'
-import { Start } from './components/Start'
-import { useQuestionsStore } from './store/questions'
-import { Game } from './components/Game'
-import { SetStateAction, useState } from 'react'
+import { Container, Stack, Typography } from "@mui/material";
+import "./App.css";
+import { Start } from "./components/Start";
+import { useQuestionsStore } from "./store/questions";
+import { Game } from "./components/Game";
+import { Header } from "./components/Header";
 
 function App() {
-  const questions = useQuestionsStore(state => state.questions)
-  const [language, setLanguage] = useState('javascript')
+  const questions = useQuestionsStore((state) => state.questions);
 
-
-  const handleProgrammingLanguageChange = (event: { target: { value: SetStateAction<string> } }) => {
-    setLanguage(event.target.value)
-  }
-  
   return (
     <main>
-      <Container maxWidth='sm'>
-        <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
-          <Select
-            value={language}
-            onChange={handleProgrammingLanguageChange}
-            displayEmpty
-            inputProps={{ 'aria-label': 'programming language' }}
-          >
-            <MenuItem value='javascript'>JavaScript</MenuItem>
-          </Select>
-          <Stack direction='row' gap={2} alignItems='center'>
-            <Typography variant='h2' component='h1'>
-              Code Quizz
-            </Typography>
-          </Stack>
+      <Header />
+      <Container maxWidth="sm" sx={{ marginTop: '16px' }}>
+        <Stack
+          direction="row"
+          gap={2}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <img
+            style={{ width: "68px", height: "68px" }}
+            src="/icon.svg"
+            alt="app icon"
+          />
+          <Typography variant="h2" component="h1" sx={{ marginBottom: '16px' }}>
+            Code Quizz
+          </Typography>
         </Stack>
         {questions.length === 0 ? <Start /> : <Game />}
       </Container>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import { Button, MenuItem, Select, Stack, Box } from "@mui/material";
+import { SetStateAction, useState } from "react";
+import { GitHubIcon } from "./icons/GitHubIcon";
+
+export const Header = () => {
+  const [language, setLanguage] = useState("javascript");
+
+  const handleProgrammingLanguageChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setLanguage(event.target.value);
+  };
+
+  return (
+    <Box width="100%">
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Select
+          value={language}
+          onChange={handleProgrammingLanguageChange}
+          displayEmpty
+          size="small"
+          inputProps={{ "aria-label": "programming language" }}
+        >
+          <MenuItem value="javascript">JavaScript</MenuItem>
+        </Select>
+        <Button
+          variant="outlined"
+          startIcon={<GitHubIcon />}
+          href="https://github.com/isaias-alt/code-quizz"
+          target="_blank"
+        >
+          Start on GitHub
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
