24f3bee73db8c99e5f9e826939b9c70928f06ba0
isaias-at <cascolucasisaias@gmail.com>
2024-06-24 23:16:09 -0400

    fix button typo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ export const Header = () => {
           href="https://github.com/isaias-alt/code-quizz"
           target="_blank"
         >
-          Start on GitHub
+          Star on GitHub
         </Button>
       </Stack>
     </Box>
