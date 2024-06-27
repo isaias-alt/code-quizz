a1af5ac58306eb54bcb563a4b820ad3e43fec5bf
isaias-at <cascolucasisaias@gmail.com>
2024-06-26 23:08:56 -0400

    fix fetch url

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,6 +1,6 @@
 import { Question } from "../types.d";
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5173/'
 
 export const getAllQuestions = async (limit: number, language: string): Promise<Question[]> => {
   const res = await fetch(`${API_URL}/${language}.json`);
