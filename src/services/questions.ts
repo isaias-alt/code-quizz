65c54c2b68bc6f97ade236a6b3542107a4ad834d
isaias-at <cascolucasisaias@gmail.com>
2024-06-26 22:56:46 -0400

    fix fetch url

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,7 +1,9 @@
 import { Question } from "../types.d";
 
+const API_URL = import.meta.env.VITE_API_URL
+
 export const getAllQuestions = async (limit: number, language: string): Promise<Question[]> => {
-  const res = await fetch(`http://localhost:5173/${language}.json`);
+  const res = await fetch(`${API_URL}/${language}.json`);
   const data: Question[] = await res.json();
   return data.sort(() => Math.random() - 0.5).slice(0, limit);
 };
