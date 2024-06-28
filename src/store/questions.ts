791b3065c9a9264ae203af5db7d0862733334e33
isaias-at <cascolucasisaias@gmail.com>
2024-06-28 00:19:31 -0400

    refactor questions store

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,21 +1,9 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { Question } from "../types.d";
-import confetti from 'canvas-confetti'
+import { State } from "../types.d";
 import { getAllQuestions } from "../services/questions";
+import { handleSelectAnswer } from '../utils/selectAnswerUtils';
 
-interface State {
-  questions: Question[]
-  currentQuestion: number
-  selectedLanguage: string
-  correctAnswersCount: number
-  fetchQuestions: (limit?: number, language?: string) => Promise<void>
-  selectAnswer: (questionId: number, answerIndex: number) => void
-  goNextQuestion: () => void
-  goPreviousQuestion: () => void
-  setSelectedLanguage: (language: string) => void
-  reset: () => void
-}
 
 export const useQuestionsStore = create<State>()(persist((set, get) => {
   return {
@@ -28,52 +16,7 @@ export const useQuestionsStore = create<State>()(persist((set, get) => {
       set({ questions });
     },
     selectAnswer: (questionId, answerIndex) => {
-      const { questions, currentQuestion, correctAnswersCount } = get();
-      const newQuestions = structuredClone(questions);
-      const questionIndex = newQuestions.findIndex(question => question.id === questionId);
-      const questionInfo =  newQuestions[questionIndex];
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
-
-      if (isCorrectUserAnswer) {
-        confetti();
-        set({ correctAnswersCount: correctAnswersCount + 1 });
-
-        if (correctAnswersCount + 1 === 10) {
-          let duration = 15 * 1000;
-          let animationEnd = Date.now() + duration;
-          let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-          function randomInRange(min: number, max: number) {
-            return Math.random() * (max - min) + min;
-          }
-
-          let interval = setInterval(function() {
-            let timeLeft = animationEnd - Date.now();
-
-            if (timeLeft <= 0) {
-              return clearInterval(interval);
-            }
-
-            let particleCount = 50 * (timeLeft / duration);
-            confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-            confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
-          }, 250);
-        }
-
-        setTimeout(() => {
-          const nextQuestion = currentQuestion + 1;
-          if (nextQuestion < questions.length) {
-            set({ currentQuestion: nextQuestion });
-          }
-        }, 800);
-      }
-
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex
-      };
-      set({ questions: newQuestions });
+      handleSelectAnswer(questionId, answerIndex, get, set);
     },
     goNextQuestion: () => {
       const { currentQuestion, questions } = get();
