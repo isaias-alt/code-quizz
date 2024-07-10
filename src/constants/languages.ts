e08cab4eb6aa65e83fc67b14f14c571ee90663a3
isaias-at <cascolucasisaias@gmail.com>
2024-07-10 16:15:00 -0400

    add java questions

--- src/constants/languages.ts
+++ src/constants/languages.ts
@@ -6,4 +6,5 @@ export const LANGUAGES: Language[] = [
   { value: 'python', label: 'Python' },
   { value: 'typescript', label: 'TypeScript' },
   { value: 'c', label: 'C' },
+  { value: 'java', label: 'Java' },
 ];
