90e632a993ab2f1e8902e6572e4c4ef3d8b944c3
isaias-at <cascolucasisaias@gmail.com>
2024-07-21 16:22:08 -0400

    improve url

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,9 +1,11 @@
 import { Question } from "../types.d"
 
-const API_URL = import.meta.env.VITE_API_URL
+const IS_PROD = import.meta.env.PROD
 
-export const getAllQuestions = async (limit: number, language: string): Promise<Question[]> => {
-  const res = await fetch(`${API_URL}/${language}.json`)
+const BASE_URL = IS_PROD ? 'https://code-quizz.vercel.app' : 'http://localhost:5173'
+
+export const getAllQuestions = async (limit: number, language: string) => {
+  const res = await fetch(`${BASE_URL}/${language}.json`)
   const data: Question[] = await res.json()
   return data.sort(() => Math.random() - 0.5).slice(0, limit)
 }
