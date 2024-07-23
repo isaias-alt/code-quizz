39ce227945a57496c3e9ea0e45cc912d58b26d4f
isaias-at <cascolucasisaias@gmail.com>
2024-07-23 01:55:30 -0400

    format

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,8 +1,8 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { State } from "../types.d";
-import { getAllQuestions } from "../services/questions";
-import { handleSelectAnswer } from "../utils";
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+import { State } from "../types.d"
+import { getAllQuestions } from "../services/questions"
+import { handleSelectAnswer } from "../utils"
 
 
 export const useQuestionsStore = create<State>()(persist((set, get) => {
@@ -12,34 +12,34 @@ export const useQuestionsStore = create<State>()(persist((set, get) => {
     currentQuestion: 0,
     correctAnswersCount: 0,
     fetchQuestions: async (limit = 10, language = get().selectedLanguage) => {
-      const questions = await getAllQuestions(limit, language);
-      set({ questions });
+      const questions = await getAllQuestions(limit, language)
+      set({ questions })
     },
     selectAnswer: (questionId, answerIndex) => {
-      handleSelectAnswer(questionId, answerIndex, get, set);
+      handleSelectAnswer(questionId, answerIndex, get, set)
     },
     goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-      const nextQuestion = currentQuestion + 1;
+      const { currentQuestion, questions } = get()
+      const nextQuestion = currentQuestion + 1
 
       if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion });
+        set({ currentQuestion: nextQuestion })
       }
     },
     goPreviousQuestion: () => {
-      const { currentQuestion } = get();
-      const previousQuestion = currentQuestion - 1;
+      const { currentQuestion } = get()
+      const previousQuestion = currentQuestion - 1
       if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion });
+        set({ currentQuestion: previousQuestion })
       }
     },
     reset: () => {
-      set({ currentQuestion: 0, questions: [], correctAnswersCount: 0 });
+      set({ currentQuestion: 0, questions: [], correctAnswersCount: 0 })
     },
     setSelectedLanguage: (language: string) => {
-      set({ selectedLanguage: language });
+      set({ selectedLanguage: language })
     }    
-  };
+  }
 }, {
   name: 'questions',
-}));
+}))
