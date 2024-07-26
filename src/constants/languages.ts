f6f86e9d7a2158b3bec04209d39d80565d9553de
isaias-at <cascolucasisaias@gmail.com>
2024-07-25 23:05:18 -0400

    add swift questions

--- src/constants/languages.ts
+++ src/constants/languages.ts
@@ -1,13 +1,14 @@
 import { Language } from "../types.d";
 
 export const LANGUAGES: Language[] = [
-  { value: 'javascript', label: 'JavaScript' },
-  { value: 'dart', label: 'Dart' },
-  { value: 'python', label: 'Python' },
-  { value: 'typescript', label: 'TypeScript' },
-  { value: 'c', label: 'C' },
-  { value: 'java', label: 'Java' },
-  { value: 'cpp', label: 'C++' },
-  { value: 'cs', label: 'C#' },
-  { value: 'php', label: 'PHP' }, 
+  { value: "javascript", label: "JavaScript" },
+  { value: "dart", label: "Dart" },
+  { value: "python", label: "Python" },
+  { value: "typescript", label: "TypeScript" },
+  { value: "c", label: "C" },
+  { value: "java", label: "Java" },
+  { value: "cpp", label: "C++" },
+  { value: "cs", label: "C#" },
+  { value: "php", label: "PHP" },
+  { value: "swift", label: "Swift" },
 ];
