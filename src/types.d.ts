5ba3a98785f640b4116a24cb9efc323a48305e32
isaias-at <cascolucasisaias@gmail.com>
2024-07-31 01:17:11 -0400

    delete unneccesary interface

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,27 +1,22 @@
 export interface Question {
-  id: number
-  question: string
-  code: string
-  answers: string[]
-  correctAnswer: number
-  userSelectedAnswer?: number
-  isCorrectUserAnswer?: boolean
+  id: number;
+  question: string;
+  code: string;
+  answers: string[];
+  correctAnswer: number;
+  userSelectedAnswer?: number;
+  isCorrectUserAnswer?: boolean;
 }
 
 export interface State {
-  questions: Question[]
-  currentQuestion: number
-  selectedLanguage: string
-  correctAnswersCount: number
-  fetchQuestions: (limit?: number, language?: string) => Promise<void>
-  selectAnswer: (questionId: number, answerIndex: number) => void
-  goNextQuestion: () => void
-  goPreviousQuestion: () => void
-  setSelectedLanguage: (language: string) => void
-  reset: () => void
-}
-
-export interface Language {
-  value: string;
-  label: string;
+  questions: Question[];
+  currentQuestion: number;
+  selectedLanguage: string;
+  correctAnswersCount: number;
+  fetchQuestions: (limit?: number, language?: string) => Promise<void>;
+  selectAnswer: (questionId: number, answerIndex: number) => void;
+  goNextQuestion: () => void;
+  goPreviousQuestion: () => void;
+  setSelectedLanguage: (language: string) => void;
+  reset: () => void;
 }
